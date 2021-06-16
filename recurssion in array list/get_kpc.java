ebfc70ca0727b217b3882001a2bfb8e45ed4dc33
varunpatnabh <varunpatnabh@gmail.com>
2021-06-16 19:46:46 +0530

    COMMIT

---
+++ recurssion in array list/get_kpc.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class get_kpc {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner scn = new Scanner(System.in);
+        String str = scn.next();
+        ArrayList<String> words = getKPC(str);
+        System.out.println(words);
+    }
+    static String[] codes = {".;", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tu", "vwx", "yz"};
+    public static ArrayList<String> getKPC(String str) {
+        if(str.length() == 0){
+            ArrayList<String> bres = new ArrayList<>();
+            bres.add("");
+            return bres;
+        }
+        char ch = str.charAt(0);
+        String ros = str.substring(1);
+        ArrayList<String> rres = getKPC(ros);
+        ArrayList<String> mres = new ArrayList<>();
+        
+        String codeforch =codes[ch - '0'];
+        for(int i = 0; i < codeforch.length(); i++) {
+            char chcode = codeforch.charAt(i);
+            for(String rstr: rres){
+                mres.add(chcode + rstr);
+            }
+        }
+        return mres;
+}
+}
